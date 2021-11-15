963d1fb04572d19e98a084d0783631228196e651
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2021-11-15 18:41:38 +0300

    Refactor project file system

--- src/main/java/com/company/jsonWork/File.java
+++ src/main/java/com/company/jsonWork/File.java
@@ -6,38 +6,35 @@ import lombok.Getter;
 
 import java.util.List;
 
-public enum File {
-    FIRST(First.class, "com/company/jsonWork/Files/file1.json"),
-    SECOND(Second.class, "com/company/jsonWork/Files/file2.json"),
-    THIRD(Third.class, "com/company/jsonWork/Files/file3.json");
+enum File {
+    FIRST("com/company/jsonWork/Files/file1.json"),
+    SECOND("com/company/jsonWork/Files/file2.json"),
+    THIRD("com/company/jsonWork/Files/file3.json");
 
     @Getter
-    private final Class<?> aClass;
+    private String path;
 
-    @Getter
-    private final String path;
-
-    File(Class<?> aClass, String path) {
-        this.aClass = aClass;
+    File(String path) {
         this.path = path;
     }
 
+    public java.io.File getFileInstance(){
+        return new java.io.File(path);
+    }
+
     @Data
-    @Builder
     public static class First {
         int id;
         String text;
     }
 
     @Data
-    @Builder
     public static class Second {
         int id;
         List<String> pictures;
     }
 
     @Data
-    @Builder
     public static class Third {
         int id;
         List<String> urls;
--- src/main/java/com/company/jsonWork/FileWriteController.java
+++ src/main/java/com/company/jsonWork/FileWriteController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
@@ -15,13 +16,53 @@ public class FileWriteController implements Runnable{
 
     @Override
     public void run() {
-        synchronized (file){
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-                List objects = mapper.readValue(new java.io.File(file.getPath()), new TypeReference<List<file.>>(){});
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        switch (file){
+            case FIRST:
+                synchronized (File.FIRST){
+                    writeInFirstFile(file);
+                }
+                break;
+            case SECOND:
+                synchronized (File.SECOND){
+                    writeInSecondFile(file);
+                }
+                break;
+            case THIRD:
+                synchronized (File.THIRD){
+                    writeInThirdFile(file);
+                }
+                break;
+            default:
+                try {
+                    throw new FileNotFoundException("Unexpected file writing");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
         }
+//        synchronized (file){
+//            ObjectMapper mapper = new ObjectMapper();
+//            try {
+//                List objects = mapper.readValue(new java.io.File(file.getPath()), new TypeReference<List<file.>>(){});
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
+    private void writeInFirstFile(File file){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<File.First> objects = mapper.readValue(file.getFileInstance(), new TypeReference<List<File.First>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeInSecondFile(File file) {
+
+    }
+
+    private void writeInThirdFile(File file) {
+
     }
 }
