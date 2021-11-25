2df06e5fade16d8f571066e151efa746bb064dba
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2021-11-25 11:01:26 +0300

    Add removing old json files

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,16 +6,36 @@ import com.company.vkFeedMine.FeedElementsMine;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebElement;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
+        // Preparation
+        deleteJsonFiles();
+
         // tasks 11 - 16
         BrowserMiner browserMiner = new BrowserMiner();
         WebElement column = browserMiner.mine();
         FeedElementsMine.findRows(column).forEach(FeedElementsMine::mineFeedRowToFiles);
         browserMiner.closeBrowser();
+
         // tasks 17 - 23
         new ProcessesRunner().run();
+    }
+
+    private static void deleteJsonFiles(){
+        Stream.of("file1path", "file2path", "file3path", "outputpath").map(Properities::getProperty).map(Path::of).forEach(path -> {
+            try {
+                Files.deleteIfExists(path);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        });
     }
 }
